285751a2748a1b955c76d86ad4205f638deb85c3
Elisabeth Robson <bethrobson@gmail.com>
2017-11-11 19:40:46 -0800

    Update serverJsonp.js
    Changed port to 8080

--- serverJsonp.js
+++ serverJsonp.js
@@ -10,7 +10,7 @@ var callback;
 var log = createLog();
 updateLog();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8124;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 http.createServer(function (req, res) {
